b2ccc8df69501e3392ba9d82490837022d9ef7cc
MichelSanchez290 <michelmiranda3786@gmail.com>
2025-02-07 14:30:55 -0600

    cambios de zaqui 07 02

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,9 +42,6 @@
                 <x-button class="ms-4">
                     {{ __('Log in') }}
                 </x-button>
-                <button type="submit">
-                    hd
-                </button>
             </div>
         </form>
     </x-authentication-card>
