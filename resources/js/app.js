9478998103d1e070dc1e818281fd08f11f8171cb
MichelSanchez290 <michelmiranda3786@gmail.com>
2025-02-11 16:28:12 -0600

    cambios nav activos

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,12 @@
 import './bootstrap';
 import './../../vendor/power-components/livewire-powergrid/dist/powergrid';
 import './../../vendor/power-components/livewire-powergrid/dist/tailwind.css';
-import 'select2/dist/css/select2.css'; // Importar estilos de Select2
+//import 'select2/dist/css/select2.css'; // Importar estilos de Select2
 
-import $ from 'jquery'; // Importar jQuery
-import 'select2'; // Importar Select2
+// import $ from 'jquery'; // Importar jQuery
+// import 'select2'; // Importar Select2
 
-// Inicializar Select2
-$(document).ready(function() {
-    $('.select2').select2();
-});
+// // Inicializar Select2
+// $(document).ready(function() {
+//     $('.select2').select2();
+// });
