f7dcae1c2d2cae20f16a443fb8b1d3578d00af16
ESMART55 <oscar.garcia@cesrh.com.mx>
2025-02-13 16:53:48 -0600

    'ingreso'

---
+++ app/Livewire/Crm/Leads/Vistaprincipal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Crm\Leads;
+
+use Livewire\Component;
+
+class Vistaprincipal extends Component
+{
+    public function render()
+    {
+        return view('livewire.crm.leads.vistaprincipal');
+    }
+}
